7a880f48a2f1ba61eaa76b986a67dd5c869f20d1
hrishicodess <hrurwaghmare@outlook.com>
2023-12-27 23:59:03 +0530

    Adding files

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "# updesha" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hrishicodess/updesha.git
+git push -u origin main
